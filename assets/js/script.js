de0384e24d5307a1cf61845f284a80c563edf8f2
Rachel Denton <rdenton3@terpmail.umd.edu>
2021-12-09 21:00:38 -0500

    hide intro page when quiz starts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-introBtnEl = document.querySelector("#btn1");
+introBtnEl = document.querySelector(".intro-btn");
+introPage = document.querySelector(".intro-page");
 timerEl = document.querySelector("#showTimer")
 
 // set a timer function
 function timer() {
     var timeLeft = 10;
+    introPage.style.display = "none";
+
 
     var timeInterval = setInterval(function() {
         if (timeLeft>-1) {
@@ -14,10 +17,16 @@ function timer() {
         timerEl.textContent = "Time's Up!"
         clearInterval(timeInterval);
         }
-    
-    
       }, 1000);
+}
+
+function startQuiz() {
+    // first need the intro page to disappear
+
+
 }
 
 // when intro button is clicked, begin the timer
 introBtnEl.addEventListener("click", timer);
+// when intro button is clicked, run the quiz function
+introBtnEl.addEventListener("click",startQuiz);
